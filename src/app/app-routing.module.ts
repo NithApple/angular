b6817e8e0153e2e02a987d0a7f128046854df668
nithapple <nochphanith8@gmail.com>
2019-10-18 08:57:34 +0700

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { OrderComponent } from './order/order.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { BiographyComponent } from './biography/biography.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/order', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'addProduct', component: AddproductComponent },
+  { path: 'updateUser/:id', component: UpdateProductComponent },
+  { path: 'order', component: OrderComponent },
+  { path: 'order-history', component: OrderHistoryComponent },
+  { path: 'biography', component: BiographyComponent },
+  { path: 'contact-us', component: ContactUsComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { OrderComponent } from './order/order.component';
+
+
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { SearchPipe } from './Pipe/search.pipe';
+import { BiographyComponent } from './biography/biography.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    AddproductComponent,
+    UpdateProductComponent,
+    OrderComponent,
+    OrderHistoryComponent,
+    SearchPipe,
+    BiographyComponent,
+    ContactUsComponent,
+    SearchPipe
+
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+
+    
   ],
-  providers: [],
+ 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
